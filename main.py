4a0eed54b489120d30ccd839558ef1fbe5629039
edusilva12 <150263705+edusilva12@users.noreply.github.com>
2023-11-20 13:24:21 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from typing import Optional
+from uuid import uuid4 as uuid
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+class Producto(BaseModel):
+    id: Optional[str]
+    nombre: str
+    precio_compra: float
+    precio_venta: float
+    proveedor: str
+
+
+app = FastAPI()
+
+productos = []
+
+
+@app.get('/')
+def index():
+    return {'mensaje': 'Bienvenidos a la API de Productos'}
+
+
+@app.get('/producto')
+def obtener_productos():
+    return productos
+
+
+@app.post('/producto')
+def crear_producto(producto: Producto):
+    producto.id = str(uuid())
+    productos.append(producto)
+    return {'mensaje': 'Producto creado satisfactoriamente.'}
+
+
+@app.get('/producto/{producto_id}')
+def obtener_producto_por_id(producto_id: str):
+    resultado = list(filter(lambda p: p.id == producto_id, productos))
+
+    if len(resultado):
+        return resultado[0]
+    
+    raise HTTPException(status_code=404, detail=f'El producto con el ID {producto_id} no fue encontrado.')
+
+
+
